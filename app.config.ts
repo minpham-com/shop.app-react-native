8ca97f95cd18ee397278bfe273cd735ce2c3bdd2
Culy247 <diep.pham@amela.vn>
2023-02-14 08:34:33 +0700

    Init

---
+++ app.config.ts
@@ -0,0 +1,46 @@
+import type { ConfigType } from '@config';
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+
+import { getConfig } from './config/config.js';
+//@ts-ignore
+const appEnv = process.env.APP_ENV ?? 'development';
+
+const Config = getConfig(appEnv) as ConfigType;
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: Config.name,
+  description: `${Config.name} Min Store`,
+  slug: 'min-store',
+  version: Config.version.toString(),
+  orientation: 'portrait',
+  icon: Config.icon,
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'cover',
+    backgroundColor: '#F75469',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: Config.scheme,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: Config.foregroundImage,
+      backgroundColor: '#FFFFFF',
+    },
+    package: Config.scheme,
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: [['@bacons/link-assets', ['./assets/fonts/Inter.ttf']]],
+  extra: {
+    APP_ENV: appEnv,
+  },
+});
---
+++ docs/docs/stay-updated.md
@@ -0,0 +1,25 @@
+---
+sidebar_position: 10
+---
+
+# Stay updated with react native
+
+Make sure to check the links below to stay updated about react native news:
+
+**Websites:**
+
+- [React Native Documentation](https://reactnative.dev/)
+- [React Native Dev Blog](https://reactnative.dev/blog)
+- [React Native Directory](https://reactnative.directory/)
+- [React Native Reddit Channel](https://www.reddit.com/r/reactnative/)
+
+**Newsletters:**
+
+- [This week in react](https://thisweekinreact.com/)
+- [The React Native Newsletter](https://reactnativenewsletter.com/)
+
+**Twitter accounts to follow:**
+
+- [Evan Bacon](https://twitter.com/Baconbrix)
+- [React Native](https://twitter.com/reactnative)
+- [Expo](https://twitter.com/expo)
